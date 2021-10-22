88b9e6acd388f610fea4f2bf2d3c47702fbf0542
JobanGrewal <jobangrewal92@gmail.com>
2021-10-22 10:30:40 -0400

    add sout statement

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Joban");
         System.out.println("Grewal");
+        System.out.println("update");
     }
 }
